4356ab0dd6fd123d7f652022f1e7d5e333d2ffa3
Pathipati chanukya Chowdary <pathipatichanukyachowdary@MacBookAir.lan>
2025-05-11 19:42:15 +0100

    empty

--- web-app/variables.tf
+++
@@ -1,9 +0,0 @@
-# variable "kubernetes_namespace" {}
-
-variable "kubernetes_namespace" {
-  description = "Kubernetes namespace to deploy resources"
-  type        = string
-  default     = "web-app"
-}
-
-
